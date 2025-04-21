21c58113739ddf9c97f23fd8f80b448c317085e8
nantasit <nantasitnsboss@gmail.com>
2025-04-21 12:55:57 +0700

    init: react-native and native-wind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#030014',
+        secondary: '#151312',
+        light:{
+          100:'#D6C6FF',
+          200:'#A8B5D8',
+          300:'39CA4AB',
+        },
+        dark:{
+          100:'#221f3d',
+          200:'#0f0d23',
+        },
+        accent:'#ab8bff'
+      }
+    },
+  },
+  plugins: [],
+}
+
