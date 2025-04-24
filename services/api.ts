af3c491bb631134d23c156302be96258a8e15564
nantasit <nantasitnsboss@gmail.com>
2025-04-24 09:54:36 +0700

    feat: creade moviedetail

--- services/api.ts
+++ services/api.ts
@@ -27,16 +27,17 @@ export const fetchMovies = async({query}:{query:string})=>{
         {console.log(e)}
 }
 
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer -'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+export const fetchMoviesDetails = async(movieId:string):Promise<MovieDetails>=>{
+    try{
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+            method:'GET',
+            headers:TMDB_CONFIG.headers,
+        });
+        if(!response.ok) throw new Error('Failed to fetch movie detail');
+        const data = await response.json()
+        return data;
+    }catch(e){
+        console.log(e)
+        throw e;
+    }
+}
